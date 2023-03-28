4fa0a8a4ffc2bd692c8baf5dcf53963b2f0a037c
Manolo Gomez <vegeta.ec@gmail.com>
2023-03-27 21:54:42 -0500

    local commit sync

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,47 +1,55 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Web.Data;
 
 namespace Web;
 
 public class Program {
-    public static void Main(string[] args) {
-        var builder = WebApplication.CreateBuilder(args);
+	public static void Main(string[] args) {
+		var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+		// Add services to the container.
+		var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+							   throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlite(connectionString));
+		builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
+		builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+		builder.Services.AddControllersWithViews();
 
-        var app = builder.Build();
+		var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment()) {
-            app.UseMigrationsEndPoint();
-        } else {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment()) {
+			app.UseMigrationsEndPoint();
+		} else {
+			app.UseExceptionHandler("/Home/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
+		// https://stackoverflow.com/questions/70511588/how-to-enable-cors-in-asp-net-core-6-0-web-api-project
+		app.UseCors(builder => builder
+			.AllowAnyHeader()
+			.AllowAnyMethod()
+			.SetIsOriginAllowed((host) => true)
+			.AllowCredentials()
+		);
 
-        app.UseRouting();
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
 
-        app.UseAuthorization();
+		app.UseRouting();
 
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();
+		app.UseAuthorization();
 
-        app.Run();
-    }
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{controller=Home}/{action=Index}/{id?}");
+		app.MapRazorPages();
+
+		app.Run();
+	}
 }
